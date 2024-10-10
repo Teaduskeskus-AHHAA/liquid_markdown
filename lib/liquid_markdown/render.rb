ed975713fb898f9adaa84c4d485f0b8044f69816
Rando Hinn <randohinn@gmail.com>
2024-10-10 21:58:10 +0300

    Update render.rb

--- lib/liquid_markdown/render.rb
+++ lib/liquid_markdown/render.rb
@@ -30,9 +30,9 @@ module LiquidMarkdown
 
     def liquidize
       t = Liquid::Template.parse(@template)
-          .render(@liquid_hash, @liquid_settings, global_filter: @global_filter_proc)
+      s = t.render(@liquid_hash, @liquid_settings, global_filter: @global_filter_proc)
       puts t.errors
-      t
+      s
     end
 
     def insert_into_template(rendered_content)
